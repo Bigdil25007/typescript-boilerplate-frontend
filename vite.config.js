3614de93b0a70dfe1a30b797a3c411d962bfa706
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-11-14 18:28:34 +0100

    fix ts error with vitest types

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,20 @@ export default defineConfig({
       "@public": path.resolve(__dirname, "public"),
     }, // Must be sync with tsconfig.json and eslint.config.js
   },
+  // Vitest configuration
+  test: {
+    globals: true, // No need to import describe, it, etc.
+    environment: "jsdom", // Browser-like environment for frontend tests (access to the DOM)
+    include: ["src/**/*.test.{ts,tsx}"],
+    exclude: ["node_modules", "dist", "coverage", "public"],
+    coverage: {
+      provider: "v8", // Use v8 for code coverage
+      reporter: ["html"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/**/*.d.ts"],
+    },
+    testTimeout: 30000, // Global timeout (in milliseconds)
+    maxConcurrency: 5, // Maximum number of tests to run in parallel
+  },
 });
